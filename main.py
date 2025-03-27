dcd15d151ec866ff8e33cd8dda20aabc67c08d79
therealpeppy <furghadmanar@gmail.com>
2025-03-27 18:06:34 -0500

    add main loop and id checker

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import requests_html
 session = requests_html.HTMLSession()
 
+run = True
+
 class Paste:
     def __init__(self, id = "GzSLsUsw"):
         self.id = id
         self.url = f"https://pastebin.com/{self.id}"
         self.content = session.get(self.url)
+        self.valid = self.content.html.find(".content__title")[0].text != "Not Found (#404)"
 
-paste = Paste(input("What's the id of the paste? "))
-print(paste.content.html.find(".source")[0].text)
+    def getTextContent(self):
+        return self.content.html.find()
+
+while run:
+    paste = Paste(input("What's the id of the paste? "))
+    while not paste.valid:
+        print("Invalid paste ID.")
+        paste = Paste(input("What's the id of the paste? "))
+    print(paste.valid)
