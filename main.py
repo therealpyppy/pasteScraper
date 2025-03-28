cbe36b161d0c2cc6e0ed7a3b2811b47fa09537aa
therealpeppy <furghadmanar@gmail.com>
2025-03-28 16:16:52 -0500

    starting password

--- main.py
+++ main.py
@@ -4,18 +4,42 @@ session = requests_html.HTMLSession()
 run = True
 
 class Paste:
-    def __init__(self, id = "GzSLsUsw"):
+    def __init__(self, id = "UqsxYknj", data = None, headers = None):
+        if id == None:
+            id = "UqsxYknj"
         self.id = id
+        self.data = data
+        self.headers = headers
         self.url = f"https://pastebin.com/{self.id}"
-        self.content = session.get(self.url)
+        self.content = session.get(self.url, headers=self.headers, data=self.data)
         self.valid = self.content.html.find(".content__title")[0].text != "Not Found (#404)"
+        self.passwordProtected = self.content.html.find(".content__title")[0].text == "Locked Paste"
 
     def getTextContent(self):
         return self.content.html.find()
 
 while run:
-    paste = Paste(input("What's the id of the paste? "))
-    while not paste.valid:
-        print("Invalid paste ID.")
-        paste = Paste(input("What's the id of the paste? "))
-    print(paste.valid)
+    id = input("What's the id of the paste? ")
+    id = None if id == "" or id == " " else id
+    print(id)
+    paste = Paste(id)
+    try:
+        while not paste.valid:
+            print("Invalid paste ID.\n")
+            id = input("What's the id of the paste? ")
+            id = None if id == "" or id == " " else id
+            paste = Paste(id)
+        
+        while paste.passwordProtected:
+            password = input("Password: ")
+            csrf_token = paste.content.html.find('input[name="_csrf-frontend"]', first=True).attrs['value']
+            data = {
+                "_csrf-frontend": csrf_token,
+                "is_burn": "1",
+                "PostPasswordVerificationForm[password]": password
+            }
+            paste = Paste(id, data=data)
+            if paste.passwordProtected:
+                print("Incorrect Password!\n")
+    except:
+        print(paste.content.html.text)
